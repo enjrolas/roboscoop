3e3303d990e3a386578551c6a403c0a0de9e1e98
enjrolas <alex@nublabs.com>
2009-03-26 18:45:08 -0400

    first commit

---
+++ basic control/primitives.py
@@ -0,0 +1,17 @@
+import sys, serial
+
+def init():
+    ser.serial.Serial(0)
+
+def forward(speed):
+
+def backward(speed):
+
+def turn(radius):
+
+def sharpTurn(speed):
+
+def quit():
+    ser.close()
+    
+
