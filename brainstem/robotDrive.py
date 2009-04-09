4c301c9dbe5ee6e790ede2e2168d3377c5af1b5f
wbosworth <bosworth@nublabs.com>
2009-04-09 14:39:30 -0400

    robotDrive.py is working beautifully in position mode

--- brainstem/robotDrive.py
+++ brainstem/robotDrive.py
@@ -2,16 +2,19 @@ import serial
 import time
 
 '''
-To use this class, you'd create a motor object:
-myMotor=motor()
+To use this class, you'd create a robotDrive object:
+robot=robotDrive()
 
-To tell the motor to move to a position target (i.e. 'go 200 clicks'), 
+To tell the robot to move to a position target (i.e. 'go 200 clicks'),
+type robot.position(200)
+
+to move at a certain velocity (4 clicks/sec), type robot.velocity(4)
 '''
 
 class robotDrive:
     def __init__(self):
-        self.front=serial.Serial( '/dev/tty.usbserial-A60087za' ,9600,timeout=0)
-        self.back=serial.Serial( '/dev/tty.usbserial-A60087PJ' ,9600,timeout=0)
+        self.front=serial.Serial( 'COM5' ,9600,timeout=0)
+        self.back=serial.Serial( 'COM6' ,9600,timeout=0)
         print("serial port open")
 
 #initialized position feedback mode
@@ -33,50 +36,51 @@ class robotDrive:
         self.front.write(initString)
         self.back.write(initString)
         print("In velocity mode")
-        
+
+    #return the 15-bit bitwise inverted representation of a number
+    def invert(self, number):
+        inverse=32767
+        for exponent in range(15,-1,-1):
+            if(number>=2**exponent):
+                inverse-=2**exponent
+                number-=2**exponent
+        return inverse
+    
+    #so--the brainstem docs are wrong about how the number going to the stem
+    #is represented--it's just a 2s complement negative number, not a weird bitshifted
+    #number they describe
     def leftPosition(self,setpoint):
-        self.positionMode()
         if setpoint<0:
             setpoint*=-1
-            param1=128+int(setpoint/256)
-            param2=setpoint%256
+            setpoint-=1
+            param1=128+(self.invert(setpoint)>>8)
+            param2=(self.invert(setpoint)&255)
         else:
-            param1=int(setpoint/256)
-            param2=setpoint%256            
+            param1=setpoint>>8
+            param2=(setpoint&255)
         moveString=chr(4)+chr(4)+chr(62)+chr(1)+chr(param1)+chr(param2)
         self.front.write(moveString)
         self.back.write(moveString)
-
+        print(param1, param2)
 
     def rightPosition(self,setpoint):
-        self.positionMode()
         if setpoint<0:
             setpoint*=-1
-            param1=128+int(setpoint/256)
-            param2=setpoint%256
+            setpoint-=1
+            param1=128+(self.invert(setpoint)>>8)
+            param2=(self.invert(setpoint)&255)
         else:
-            param1=int(setpoint/256)
-            param2=setpoint%256            
+            param1=setpoint>>8
+            param2=(setpoint&255)            
         moveString=chr(4)+chr(4)+chr(62)+chr(0)+chr(param1)+chr(param2)
         self.front.write(moveString)
         self.back.write(moveString)
 
     def position(self, setpoint):  #move both motors a <setpoint> number of clicks
         self.positionMode()
-        if setpoint<0:
-            setpoint*=-1
-            param1=128+int(setpoint/256)
-            param2=setpoint%256
-        else:
-            param1=int(setpoint/256)
-            param2=setpoint%256            
-        moveString=chr(4)+chr(4)+chr(62)+chr(0)+chr(param1)+chr(param2)
-        self.front.write(moveString)
-        self.back.write(moveString)
-        moveString=chr(4)+chr(4)+chr(62)+chr(1)+chr(param1)+chr(param2)
-        self.front.write(moveString)
-        self.back.write(moveString)
-        
+        self.leftPosition(-1*setpoint)
+        self.rightPosition(setpoint)
+
     def velocity(self, velocity):
         self.velocityMode()
 
