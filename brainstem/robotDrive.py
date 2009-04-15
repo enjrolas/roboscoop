7a0cc2ffacaf4e9de232f821e27c6c4c0e998de3
wbosworth <bosworth@nublabs.com>
2009-04-15 18:10:12 -0400

    adding PID configuration

--- brainstem/robotDrive.py
+++ brainstem/robotDrive.py
@@ -12,6 +12,19 @@ to move at a certain velocity (4 clicks/sec), type robot.velocity(4)
 '''
 
 class robotDrive:
+
+    velocityP=32
+    velocityI=0
+    velocityD=5
+
+    positionP=32
+    positionI=0
+    positionD=5
+
+    rotateP=32
+    rotateI=0
+    rotateD=5
+    
     def __init__(self):
         self.front=serial.Serial( 'COM5' ,9600,timeout=0)
         self.back=serial.Serial( 'COM6' ,9600,timeout=0)
@@ -28,13 +41,49 @@ class robotDrive:
         print("In position mode")
 
 #initializes velocity feedback mode
-    def velocityMode(self):
+    def velocityMode(self, p, i, d):
         initString=chr(4)+chr(5)+chr(63)+chr(0)+chr(0)+chr(7)+chr(0)
         self.front.write(initString)
         self.back.write(initString)
         initString=chr(4)+chr(5)+chr(63)+chr(1)+chr(0)+chr(7)+chr(0)
         self.front.write(initString)
         self.back.write(initString)
+
+        highP=int(p/256)
+        lowP=p%256
+
+        highI=int(i/256)
+        lowI=i%256
+
+        highD=int(d/256)
+        lowD=d%256
+
+        #set P value
+        pString=chr(4)+chr(5)+chr(63)+chr(0)+chr(1)+chr(highP)+chr(lowP) #left side
+        self.front.write(pString)
+        self.back.write(pString)
+        pString=chr(4)+chr(5)+chr(63)+chr(1)+chr(1)+chr(highP)+chr(lowP) #right side
+        self.front.write(pString)
+        self.back.write(pString)
+
+        #set I value
+        iString=chr(4)+chr(5)+chr(63)+chr(0)+chr(1)+chr(highI)+chr(lowI) #left side
+        self.front.write(iString)
+        self.back.write(iString)
+        iString=chr(4)+chr(5)+chr(63)+chr(1)+chr(1)+chr(highI)+chr(lowI) #right side
+        self.front.write(iString)
+        self.back.write(iString)
+
+
+        #set D value
+        dString=chr(4)+chr(5)+chr(63)+chr(0)+chr(1)+chr(highD)+chr(lowD) #left side
+        self.front.write(dString)
+        self.back.write(dString)
+        dString=chr(4)+chr(5)+chr(63)+chr(1)+chr(1)+chr(highD)+chr(lowD) #right side
+        self.front.write(dString)
+        self.back.write(dString)
+
+        
         print("In velocity mode")
 
     #return the 15-bit bitwise inverted representation of a number
@@ -49,7 +98,7 @@ class robotDrive:
     #so--the brainstem docs are wrong about how the number going to the stem
     #is represented--it's just a 2s complement negative number, not a weird bitshifted
     #number they describe
-    def leftPosition(self,setpoint):
+    def leftValue(self,setpoint):
         if setpoint<0:
             setpoint*=-1
             setpoint-=1
@@ -63,7 +112,7 @@ class robotDrive:
         self.back.write(moveString)
         print(param1, param2)
 
-    def rightPosition(self,setpoint):
+    def rightValue(self,setpoint):
         if setpoint<0:
             setpoint*=-1
             setpoint-=1
@@ -76,18 +125,34 @@ class robotDrive:
         self.front.write(moveString)
         self.back.write(moveString)
 
-    def position(self, setpoint):  #move both motors a <setpoint> number of clicks
+    def position(self, setpoint, positionP, positionI, positionD):  #move both motors a <setpoint> number of clicks
         self.positionMode()
-        self.leftPosition(-1*setpoint)
-        self.rightPosition(setpoint)
+        self.leftValue(-1*setpoint)
+        self.rightValue(setpoint)
 
     def velocity(self, velocity):
+        self.velocityMode(self.velocityP, self.velocityI, self.velocityD)
+        self.leftValue(-1*velocity)
+        self.rightValue(velocity)
+
+    def rotate(self, speed, rotateP, rotateI, rotateD):
+        self.velocityMode()
+        self.leftValue(speed)
+        self.rightValue(speed)
+    
+
+    def rampConfig(self):
         self.velocityMode()
 
-
+    def eepromReset(self):
+        resetString=chr(4)+chr(16)+chr(0)+chr(0)+chr(255)
+        self.front.write(resetString)
+        resetString=chr(4)+chr(24)
+        self.front.write(resetString)
+        
     def close(self):
-        self.ser.close()
-
+        self.front.close()
+        self.back.close()
     def poop(self):
         print("poop!")
     
