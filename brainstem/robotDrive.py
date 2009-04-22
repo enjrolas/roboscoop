93d197c6f0569114558226a21caf6fba3ddaaaa7
wbosworth <bosworth@nublabs.com>
2009-04-22 12:53:17 -0400

    the robotDrive code is now totally functional

--- brainstem/robotDrive.py
+++ brainstem/robotDrive.py
@@ -8,18 +8,19 @@ myMotor=motor()
 To tell the motor to move to a position target (i.e. 'go 200 clicks'), 
 '''
 
-positionP=1
-positionI=0
-positionD=0
-
-velocityP=1
-velocityI=0
-velocityD=0
 
 class robotDrive:
     def __init__(self):
-        self.front=serial.Serial( '/dev/tty.usbserial-A60087za' ,9600,timeout=0)
-        self.back=serial.Serial( '/dev/tty.usbserial-A60087PJ' ,9600,timeout=0)
+        self.positionP=1
+        self.positionI=0
+        self.positionD=0
+
+        self.velocityP=1
+        self.velocityI=0
+        self.velocityD=0
+
+        self.front=serial.Serial( 'COM5' ,9600,timeout=0)
+        self.back=serial.Serial( 'COM6' ,9600,timeout=0)
         print("serial port open")
 
 #initialized position feedback mode
@@ -30,6 +31,7 @@ class robotDrive:
         initString=chr(4)+chr(5)+chr(63)+chr(1)+chr(0)+chr(5)+chr(0)
         self.front.write(initString)
         self.back.write(initString)
+        self.motorConfigure(self.formatVariable(self.positionP), self.formatVariable(self.positionI), self.formatVariable(self.positionD))        
         print("In position mode")
 
 #initializes velocity feedback mode
@@ -40,23 +42,28 @@ class robotDrive:
         initString=chr(4)+chr(5)+chr(63)+chr(1)+chr(0)+chr(7)+chr(0)
         self.front.write(initString)
         self.back.write(initString)
+        self.motorConfigure(self.formatVariable(self.velocityP), self.formatVariable(self.velocityI), self.formatVariable(self.velocityD))        
         print("In velocity mode")
+        
 
 
 #formats PID variables in the silly-ish brainstem format
     def formatVariable(self, variable):
         formatted=0
+        done=0
         fraction=variable-int(variable)
         for i in range(0,32):   #the bottom 5 bits of the formatted number are fractional bits representing 32ths
-            if((i/32)>fraction):
+            if((i/32)>fraction) and (done==0):
                 formatted+=i
-                goto .exit
-        label .exit
+                done=1
         formatted+=int(variable)*32
         return formatted
 
 #writes PID values to the brainstem
     def motorConfigure(self, P, I, D):
+        print P
+        print I
+        print D
         pString=chr(4)+chr(5)+chr(63)+chr(0)+chr(1)+chr(int(P/256))+chr(P%256)
         self.front.write(pString)
         self.back.write(pString)
@@ -64,17 +71,17 @@ class robotDrive:
         self.front.write(pString)
         self.back.write(pString)
 
-        iString=chr(4)+chr(5)+chr(63)+chr(0)+chr(1)+chr(int(I/256))+chr(I%256)
+        iString=chr(4)+chr(5)+chr(63)+chr(0)+chr(2)+chr(int(I/256))+chr(I%256)
         self.front.write(iString)
         self.back.write(iString)
-        iString=chr(4)+chr(5)+chr(63)+chr(1)+chr(1)+chr(int(I/256))+chr(I%256)
+        iString=chr(4)+chr(5)+chr(63)+chr(1)+chr(2)+chr(int(I/256))+chr(I%256)
         self.front.write(iString)
         self.back.write(iString)
 
-        dString=chr(4)+chr(5)+chr(63)+chr(0)+chr(1)+chr(int(D/256))+chr(D%256)
+        dString=chr(4)+chr(5)+chr(63)+chr(0)+chr(3)+chr(int(D/256))+chr(D%256)
         self.front.write(dString)
         self.back.write(dString)
-        dString=chr(4)+chr(5)+chr(63)+chr(1)+chr(1)+chr(int(D/256))+chr(D%256)
+        dString=chr(4)+chr(5)+chr(63)+chr(1)+chr(3)+chr(int(D/256))+chr(D%256)
         self.front.write(dString)
         self.back.write(dString)
         
@@ -95,7 +102,7 @@ class robotDrive:
         
     def leftValue(self,value):
         if value<0:
-            value=invert(self,-1*value)
+            value=self.invert(-1*value)
             param1=int(value/256)
             param2=value%256
         else:
@@ -108,7 +115,7 @@ class robotDrive:
 
     def rightValue(self,value):
         if value<0:
-            value=invert(self,-1*value)
+            value=self.invert(-1*value)
             param1=int(value/256)
             param2=value%256
         else:
@@ -119,20 +126,28 @@ class robotDrive:
         self.back.write(moveString)
 
     def position(self, setpoint):  #move both motors a <setpoint> number of clicks
-        self.positionMode(self)
-        self.motorConfigure(self,formatVariables(self, self.positionP), formatVariables(self, self.positionI), formatVariables(self, self.positionD))
-        leftPosition(self, setpoint)
-        rightPosition(self, setpoint)
+        self.positionMode()
+        self.leftValue(setpoint)
+        self.rightValue(-1*setpoint)
         
     def velocity(self, velocity):
-        self.velocityMode(self)
-        self.motorConfigure(self,formatVariables(self, self.velocityP), formatVariables(self, self.velocityI), formatVariables(self, self.velocityD))
-        self.leftValue(self, velocity)
-        self.rightValue(self, velocity)
+        self.velocityMode()
+        self.leftValue(velocity)
+        self.rightValue(-1*velocity)
 
+    def rotatePosition(self, angle):
+        self.positionMode()
+        self.leftValue(angle)
+        self.rightValue(angle)
+
+    def rotateVelocity(self, velocity):
+        self.velocityMode()
+        self.leftValue(velocity)
+        self.rightValue(velocity)
 
     def close(self):
-        self.ser.close()
+        self.front.close()
+        self.back.close()
 
     def poop(self):
         print("poop!")
